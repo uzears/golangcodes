4b56550a0ad120cec4ae0e9dbacc809765a9e2b8
uzear shaikh <shaikhuzear@gmail.com>
2025-12-04 21:09:35 +0530

    feat : added students-api

--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/uzears/golangcodes/tree/main/students-api
-
-go 1.23.4
--- students-api/cmd/main.go
+++ students-api/cmd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/uzears/students-api/internal/config"
 )
 
 func main() {
 
 	cfg = config.MustLoad()
+
+	router := http.NewServeMux()
+
+	http.ListenAndServe(cfg.HTTPServer.Addr, router)
+
 }
